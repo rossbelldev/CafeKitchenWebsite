29f3577dafcc081bcf25527020663885ffb564e9
theRGTgamer <rosscameronclarkbell:gmail.com>
2020-08-21 15:39:50 +0100

    finised for the day

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def root():
     return render_template("index.html")
 
+@app.route('/menu')
+def menu():
+    return render_template("menu.html")
+
+
+
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
