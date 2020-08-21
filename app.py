95352fbfa63e80298416899464ea02c8caaa951f
theRGTgamer <rosscameronclarkbell:gmail.com>
2020-08-21 14:28:11 +0100

    Added a lot today

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
-app = Flask (__name__)
+from flask import Flask, render_template
 
-@app.route("/")
-def hello():
-    return 'Hello Brunch'
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
